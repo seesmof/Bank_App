6be945e1420810a4732f3ad597dd3c0968a9d1af
seesmof <110289150+seesmof@users.noreply.github.com>
2022-10-09 15:19:06 +0300

    chore: :construction: working

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-<h2 align="center">Register/Login App</h2>
+<h1 align="center">Register/Login App</h1>
 
 <div align="center">
 
--- main.cpp
+++ main.cpp
@@ -4,33 +4,47 @@
 
 using namespace std;
 
+string username, lname, fname;
+int password, input, bday, bmonth, byear;
+double balance = 0;
+
 void REG()
 {
-    string lname, fname, city;
-    int bday, bmonth, byear;
-
-    cout << "Welcome to SkylinesBank! Thank you for choosing us!" << endl
-         << endl;
-
     cout << "Please enter your first name: " << endl;
     cin >> fname;
     cout << "Please enter your last name: " << endl;
     cin >> lname;
+    cout << "Please enter your username: " << endl;
+    cin >> username;
+    cout << "Please enter your birth date in DD MM YYYY format: ";
+    cin >> bday >> bmonth >> byear;
+
+    if (bday > 31 || byear > 2004 || bmonth > 12)
+    {
+        cout << "Please enter valid birth date." << endl;
+    }
+    else
+    {
+        cout << endl;
+        cout << "Congratulations, " << username << "! You have successfully created a new account!" << endl;
+    }
 }
 
 void LOGIN()
 {
-    string login;
-    int password;
+    cout << "Please enter your username: " << endl;
+    cin >> username;
+    cout << "Please enter your password: " << endl;
+    cin >> password;
+    cout << "Welcome back, " << username << "! Your current balance is $" << balance << endl;
 }
 
 int main()
 {
-    int input;
-
-    cout << "Welcome to SkilinesBank! Thank you for choosing us. Please follow the instructions to create your account." << endl;
+    cout << "Welcome to SkylinesBank! Thank you for choosing us" << endl;
     cout << "If you already have an account, you can login by typing 1 below. Otherwise, type 0" << endl;
     cin >> input;
+    cout << endl;
     if (input == 1)
     {
         LOGIN();
@@ -39,4 +53,6 @@ int main()
     {
         REG();
     }
+
+    return 0;
 }
